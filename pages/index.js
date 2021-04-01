badbb5d49ad293ba065d4b04c171af460e1b10cd
Qu4k3 <paul.guillamon@gmail.com>
2021-04-01 09:39:13 +0200

    basic nextjs 10+ commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tau Studio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        
+      </main>
+
+      <footer>
+        
+      </footer>
+    </>
+  )
+}
