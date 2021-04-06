871bd5ffed75cf1f0f149054acfc5c541ddfb145
Qu4k3 <paul.guillamon@gmail.com>
2021-04-06 12:08:59 +0200

    landing page logo rotation

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        
+      <main className="grid">
+        <div className="flex">
+          <img className="animated" src="/tau.svg" />
+          <img className="ml-2" src="/studio.svg" />
+        </div>
       </main>
 
       <footer>
-        
+
       </footer>
     </>
   )
