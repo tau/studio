487d5676fb38dfe9da765dc914fc085be0141e9d
Qu4k3 <paul.guillamon@gmail.com>
2021-04-06 18:43:38 +0200

    GA

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,19 @@ export default function Home() {
       <Head>
         <title>Tau Studio</title>
         <link rel="icon" href="/favicon.ico" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K04X75PRJ4"></script>
+        <script dangerouslySetInnerHTML={
+          {
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-K04X75PRJ4');
+              `
+          }
+        }>
+        </script>
       </Head>
 
       <main className="grid">
