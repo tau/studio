54d14fec55073197f477477c5ecd3a4c3c020212
Qu4k3 <paul.guillamon@gmail.com>
2021-04-15 21:28:42 +0200

    Update SiteLayout.js

--- layouts/SiteLayout.js
+++ layouts/SiteLayout.js
@@ -1,11 +1,19 @@
 import Image from 'next/image'
 import Footer from '../components/Footer'
+import Nav from '../components/nav';
 
 export default function Layout({ children }) {
 
   return (
     <>
       <section>
+        <Nav />
+
+        <br />
+        <br />
+        <br />
+        <br />
+        
         <Footer />
       </section>
     </>
