a54c01080a296ef0a4941b7bf60ef270ef52ec05
Qu4k3 <paul.guillamon@gmail.com>
2021-04-16 22:33:22 +0200

    Create Clients.js

---
+++ components/Clients.js
@@ -0,0 +1,17 @@
+export default function Clients() {
+
+  return (
+    <>
+      <section className="grid">
+        <div className="square">
+          <a target="_self">
+            <div className="square__wrapper">
+              <img className="square__image" src="" />
+              <div className="hover" style="background-color:#039be5"></div>
+            </div>            
+          </a>
+        </div>
+      </section>
+    </>
+  );
+}
