0c91d3c9821d045a88e2257d94cd5ad223e6ca44
Qu4k3 <paul.guillamon@gmail.com>
2021-04-16 22:32:17 +0200

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,44 @@
 import Head from 'next/head'
 import SiteLayout from '../layouts/SiteLayout'
+import Clients from '../components/Clients'
+import Square from '../components/Square'
+import { createClient } from 'contentful';
+import FooterNav from '../components/FooterNav';
 
-function Home() {
+const client = createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+})
+
+export async function getStaticProps() {
+  let data = await client.getEntries({
+    content_type: client
+  })
+
+  return {
+    props: {
+      clients: data.items
+    }
+  }
+}
+
+function Home({ clients }) {
+
+  console.log(clients)
   return (
     <>
       <Head>
         <meta key="robots" name="robots" content="noindex,follow" />
         <meta key="googlebot" name="googlebot" content="noindex,follow" />
       </Head>
-            
+
+      <section className="grid">        
+        {clients.map((client, i) => (
+          <Square client={client} index={i} />
+        ))}
+      </section>
+
+      <FooterNav />
     </>
   )
 }
