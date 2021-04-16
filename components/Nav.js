7d44fdcb1f47d8a28b5a870b51e1a93024907412
Qu4k3 <paul.guillamon@gmail.com>
2021-04-16 10:43:50 +0200

    Menu links css effects on hover

--- components/Nav.js
+++ components/Nav.js
@@ -1,36 +1,37 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Nav() {
+
+  const router = useRouter()
+
   return (
     <>
       <nav>
         <section className="content">
           <div className="nav__wrapper">
-            <div className="nav__logo-wrapper">
+            <div onClick={() => router.push('/')} className="nav__logo-wrapper">
               <img className="nav__logo" src="/tau.svg" />
               <img className="nav__name" src="/studio.svg" />
             </div>
             <div className="nav__flex">
               <ul className="nav__items">
-              <li>
-                  
-                </li>
-                <li>
+                <li className="c1">
                   <Link href="/trabajos">
                     <a>Trabajos</a>
                   </Link>
                 </li>
-                <li>
+                <li className="c2">
                   <Link href="/proyectos">
                     <a>Proyectos</a>
                   </Link>
                 </li>
-                <li>
+                <li className="c3">
                   <Link href="/nosotros">
                     <a>El estudio</a>
                   </Link>
                 </li>
-                <li>
+                <li className="c4">
                   <Link href="/contacto">
                     <a>Contacto</a>
                   </Link>
