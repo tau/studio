3edc791df789ca40c2483e9a4d6a7e9f9d6d2d1d
Qu4k3 <paul.guillamon@gmail.com>
2021-04-18 21:47:49 +0200

    Update Clients.js

--- components/Clients.js
+++ components/Clients.js
@@ -1,16 +1,21 @@
-export default function Clients() {
+import Square from './Square'
+
+export default function Clients({clients, max}) {
+
+  if(!max) {
+    max = 14;
+  }  
 
   return (
     <>
-      <section className="grid">
-        <div className="square">
-          <a target="_self">
-            <div className="square__wrapper">
-              <img className="square__image" src="" />
-              <div className="hover" style="background-color:#039be5"></div>
-            </div>            
-          </a>
-        </div>
+      <section>
+        <section className="content">
+          <section className="square-grid">        
+            {clients.slice(0, max).map((client, i) => (
+              <Square client={client} index={i} />
+            ))}
+          </section>
+        </section>
       </section>
     </>
   );
